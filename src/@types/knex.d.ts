e2090aae74282faf8dee305b7dbadc06f19bb266
Ricardo Vinciguerra <ricardorhv.dev@gmail.com>
2024-10-21 08:38:48 -0300

    chore: type tables in knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      type: string
+      session_id?: string
+      created_at: Date
+    }
+  }
+}
